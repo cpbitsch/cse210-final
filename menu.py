0ae2ca2772d55cb2f1d18b3ffe8f9cdb429aab9d
Jacob Running <86081584+Twister1M@users.noreply.github.com>
2022-07-14 18:34:19 -0600

    Update menu.py
    Basic Implementation example: https://pygame-menu.readthedocs.io/en/4.2.8/

--- menu.py
+++ menu.py
@@ -1,1 +1,21 @@
+import pygame
+import pygame_menu
 
+pygame.init()
+surface = pygame.display.set_mode((800,600))
+
+def start_the_game():
+    # Do the job here !
+    pass
+
+def help_menu():
+    # Do the job here !
+    pass
+
+menu = pygame_menu.Menu('Space Invaders', 600, 500,
+                       theme=pygame_menu.themes.THEME_BLUE)
+
+menu.add.text_input('Input Name :', default='')
+menu.add.button('Play', start_the_game)
+menu.add.button('Help', help_menu)
+menu.add.button('Quit', pygame_menu.events.EXIT)
